4b97cd4c850447873eb9639b7d8a1d3e6a44163a
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2023-08-28 13:50:28 -0500

    Fetch from JSON

--- scripts/game.js
+++ scripts/game.js
@@ -16,16 +16,27 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
+let questions = [];
+
+fetch("../data/questions.json").then(res => {
+    return res.json();
+}).then(loadedQuestions => {
+    questions = loadedQuestions;
+    startGame();
+});
+
 //CONSTANTS
 const CORRECT_BONUS = 10; //How many points we want to give for each correct answer
 const MAX_QUESTIONS = 3; //How many questions we want in our game
 const FEEDBACK_DELAY = 50; //How long to show feedback
 
+
+
+
 startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions]; //Spread operator to copy the questions array
-    console.log(availableQuestions);
     getNewQuestion();
 }
 
@@ -95,34 +106,3 @@ incrementScore = num => {
     score += num;
     txtScore.innerText = score;
 }
-
-let questions = [
-    {
-      question: "Inside which HTML element do we put the JavaScript??",
-      choice1: "<script>",
-      choice2: "<javascript>",
-      choice3: "<js>",
-      choice4: "<scripting>",
-      answer: 1
-    },
-    {
-      question:
-        "What is the correct syntax for referring to an external script called 'xxx.js'?",
-      choice1: "<script href='xxx.js'>",
-      choice2: "<script name='xxx.js'>",
-      choice3: "<script src='xxx.js'>",
-      choice4: "<script file='xxx.js'>",
-      answer: 3
-    },
-    {
-      question: " How do you write 'Hello World' in an alert box?",
-      choice1: "msgBox('Hello World');",
-      choice2: "alertBox('Hello World');",
-      choice3: "msg('Hello World');",
-      choice4: "alert('Hello World');",
-      answer: 4
-    }
-  ];
-  
-  
-startGame();
